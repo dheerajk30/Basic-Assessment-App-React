85f4839eb73152e5fa062b7fff314c3b9b882312
Dheeraj Khathuria <khathuriadheeraj@gmail.com>
2020-08-16 16:19:16 +0530

    Updates

---
+++ precache-manifest.dadf64b7959668fe0f1884267aafa897.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d2ffcb6b7a3cf878582fadbf6722f75",
+    "url": "/Basic-Assessment-App-React/index.html"
+  },
+  {
+    "revision": "3d495aafb0fb4649c793",
+    "url": "/Basic-Assessment-App-React/static/css/main.b0dc0b06.chunk.css"
+  },
+  {
+    "revision": "f49c79a24b4d77d455be",
+    "url": "/Basic-Assessment-App-React/static/js/2.df944c40.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Basic-Assessment-App-React/static/js/2.df944c40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d495aafb0fb4649c793",
+    "url": "/Basic-Assessment-App-React/static/js/main.a82301e4.chunk.js"
+  },
+  {
+    "revision": "c73f72d7d60204156b20",
+    "url": "/Basic-Assessment-App-React/static/js/runtime-main.1d9d933a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Basic-Assessment-App-React/static/media/logo.5d5d9eef.svg"
+  }
+]);
